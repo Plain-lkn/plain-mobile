a3d3692870b296036bba97d3e7164a170084d36f
Dev Ttangkong <ttankkeo112@gmail.com>
2025-02-23 11:55:21 +0900

    fix: update flutter_appbar package to fixed an issue

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart' hide Theme;
 import 'package:flutter_plain_application/components/settings/setting_binding.dart';
 import 'package:flutter_plain_application/components/settings/theme.dart';
-import 'package:flutter_plain_application/pages/root.dart';
 import 'package:flutter_plain_application/widgets/appbar_connection_with_header.dart';
 import 'package:flutter_rebuildable/flutter_rebuildable.dart';
 import 'package:flutter_touch_ripple/widgets/touch_ripple.dart';
